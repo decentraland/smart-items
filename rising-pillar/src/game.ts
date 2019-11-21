a3d279a792d218be26e31890530fde6a067b0dae
nearnshaw <nearnshaw@decentraland.org>
2019-11-21 18:37:10 -0300

    pillar first draft

---
+++ rising-pillar/src/game.ts
@@ -0,0 +1,13 @@
+import { Spawner } from '../node_modules/decentraland-builder-scripts/spawner'
+import Platform, { Props } from './item'
+
+const pillar = new Platform()
+const spawner = new Spawner<Props>(pillar)
+
+spawner.spawn('pillar', new Transform({ position: new Vector3(8, 0, 8) }), {
+  height: 10,
+  speed: 5,
+  autoStart: true,
+  onReachBottom: [{ entityName: 'pillar', actionId: 'goToTop', values: {} }],
+  onReachTop: [{ entityName: 'pillar', actionId: 'goToBottom', values: {} }]
+})
