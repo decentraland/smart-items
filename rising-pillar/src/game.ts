a69b3976f2d0f067b7ead8ff5301fe7bfa8cc4eb
nearnshaw <nearnshaw@decentraland.org>
2019-11-22 18:44:16 -0300

    size

--- rising-pillar/src/game.ts
+++ rising-pillar/src/game.ts
@@ -5,7 +5,7 @@ const pillar = new Platform()
 const spawner = new Spawner<Props>(pillar)
 
 spawner.spawn('pillar', new Transform({ position: new Vector3(8, 0, 8) }), {
-  height: 15,
+  height: 5.5,
   speed: 5,
   autoStart: true,
   onReachBottom: [{ entityName: 'pillar', actionId: 'rise', values: {} }],
