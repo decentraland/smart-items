fe5efe3d929a34b708bb5327651efbed9bfd64b6
Juan Cazala <juan@decentraland.org>
2019-11-22 15:43:35 -0300

    chore: linted and some renaming

--- padlock/src/game.ts
+++ padlock/src/game.ts
@@ -4,9 +4,7 @@ import PadLock, { Props } from './item'
 const padLock = new PadLock()
 const spawner = new Spawner<Props>(padLock)
 
-spawner.spawn(
-	'padLock', 
-	new Transform({ position: new Vector3(4, 1.7, 8) }),  {
-	combination : 1234,
-	onSolve : null
+spawner.spawn('padLock', new Transform({ position: new Vector3(4, 1.7, 8) }), {
+  combination: 1234,
+  onSolve: null
 })
