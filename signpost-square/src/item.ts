f0c4e4540a571ac8cd76d23297a49ce427c1516d
nearnshaw <nearnshaw@decentraland.org>
2019-11-26 13:15:40 -0300

    signposts genesis

---
+++ signpost-square/src/item.ts
@@ -0,0 +1,38 @@
+export type Props = {
+  text?: string
+  fontSize?: number
+}
+
+export default class SignPost implements IScript<Props> {
+  init() {}
+
+  spawn(host: Entity, props: Props, channel: IChannel) {
+    const sign = new Entity()
+    sign.setParent(host)
+
+    sign.addComponent(new GLTFShape('models/signpost/SignPost_Square.glb'))
+
+    let signText = new Entity()
+    signText.setParent(host)
+    let text = new TextShape(props.text)
+    text.fontSize = props.fontSize
+    text.color = Color3.Black()
+
+    text.outlineColor = Color3.Black()
+    text.outlineWidth = 0.3
+
+    text.width = 20
+    text.height = 10
+    text.hTextAlign = 'center'
+
+    signText.addComponent(text)
+
+    signText.addComponent(
+      new Transform({
+        position: new Vector3(0, 2.05, -0.03),
+        rotation: Quaternion.Euler(0, 0, 0),
+        scale: new Vector3(0.05, 0.05, 0.05)
+      })
+    )
+  }
+}
---
+++ signpost-street-names/src/item.ts
@@ -0,0 +1,83 @@
+export type Props = {
+  textTop?: string
+  textLower?: string
+  fontSize?: number
+}
+
+export default class SignPost implements IScript<Props> {
+  init() {}
+
+  spawn(host: Entity, props: Props, channel: IChannel) {
+    const sign = new Entity()
+    sign.setParent(host)
+
+    sign.addComponent(
+      new GLTFShape('models/signpost/SignPost_Street_Names.glb')
+    )
+
+    let signTextTop = new Entity()
+    signTextTop.setParent(host)
+    let text1 = new TextShape(props.textLower)
+    text1.fontSize = props.fontSize
+    text1.color = Color3.Black()
+
+    text1.width = 20
+    text1.height = 10
+    text1.hTextAlign = 'center'
+
+    signTextTop.addComponent(text1)
+
+    signTextTop.addComponent(
+      new Transform({
+        position: new Vector3(-0.03, 1.725, 0.55),
+        rotation: Quaternion.Euler(0, 90, 0),
+        scale: new Vector3(0.05, 0.05, 0.05)
+      })
+    )
+
+    let signTextTop2 = new Entity()
+    signTextTop2.setParent(host)
+    signTextTop2.addComponent(text1)
+
+    signTextTop2.addComponent(
+      new Transform({
+        position: new Vector3(0.03, 1.725, 0.55),
+        rotation: Quaternion.Euler(0, 270, 0),
+        scale: new Vector3(0.05, 0.05, 0.05)
+      })
+    )
+
+    let signTextLower = new Entity()
+    signTextLower.setParent(host)
+    let text2 = new TextShape(props.textTop)
+    text2.fontSize = props.fontSize
+    text2.color = Color3.Black()
+
+    text2.width = 20
+    text2.height = 10
+    text2.hTextAlign = 'center'
+
+    signTextLower.addComponent(text2)
+
+    signTextLower.addComponent(
+      new Transform({
+        position: new Vector3(-0.55, 2.025, 0.03),
+        rotation: Quaternion.Euler(0, 180, 0),
+        scale: new Vector3(0.05, 0.05, 0.05)
+      })
+    )
+
+    let signTextLower2 = new Entity()
+    signTextLower2.setParent(host)
+
+    signTextLower2.addComponent(text2)
+
+    signTextLower2.addComponent(
+      new Transform({
+        position: new Vector3(-0.55, 2.025, -0.03),
+        rotation: Quaternion.Euler(0, 0, 0),
+        scale: new Vector3(0.05, 0.05, 0.05)
+      })
+    )
+  }
+}
