6cc29b365088151745b2b89e935535b8e6c8a06b
nearnshaw <nearnshaw@decentraland.org>
2019-11-28 15:57:37 -0300

    first draft

---
+++ message_bubble/src/game.ts
@@ -0,0 +1,10 @@
+import { Spawner } from '../node_modules/decentraland-builder-scripts/spawner'
+import MessageBubble, { Props } from './item'
+
+const post = new MessageBubble()
+const spawner = new Spawner<Props>(post)
+
+spawner.spawn('bubble', new Transform({ position: new Vector3(4, 1, 8) }), {
+  text: 'Juancalandia',
+  fontSize: 30
+})
---
+++ message_bubble/src/item.ts
@@ -0,0 +1,96 @@
+import { BubbleSystem, MessageBubbleComponent } from './bubble'
+
+export type Props = {
+  text?: string
+  fontSize?: number
+}
+
+type ChangeTextType = {
+  newText: string
+}
+
+export default class MessageBubble implements IScript<Props> {
+  openClip = new AudioClip('sounds/MessageBubble.mp3')
+  init() {
+    engine.addSystem(new BubbleSystem())
+  }
+
+  toggle(bubble: Entity, text: TextShape, value: boolean, playSound = true) {
+    let bubbleData = bubble.getComponent(MessageBubbleComponent)
+
+    if (bubbleData.isOpen === value) return
+
+    if (playSound) {
+      const source = new AudioSource(this.openClip)
+      bubble.addComponentOrReplace(source)
+      source.playing = true
+    }
+
+    bubbleData.isOpen = value
+    bubbleData.transition = 0
+
+    if (value) {
+      text.value = bubbleData.text
+    } else {
+      text.value = ''
+    }
+    log(bubbleData.isOpen)
+  }
+
+  spawn(host: Entity, props: Props, channel: IChannel) {
+    const bubble = new Entity()
+    bubble.setParent(host)
+
+    bubble.addComponent(new GLTFShape('models/bubble/MessageBubble.glb'))
+
+    bubble.addComponent(
+      new Transform({
+        position: new Vector3(0, 0, 0),
+        scale: new Vector3(0.3, 0.3, 0.3)
+      })
+    )
+
+    let bubbleData = new MessageBubbleComponent(channel, props.text)
+    bubble.addComponent(bubbleData)
+
+    let signText = new Entity()
+    signText.setParent(bubble)
+    let text = new TextShape('')
+    text.fontSize = props.fontSize
+    text.color = Color3.FromHexString('#8cfdff')
+    text.outlineWidth = 0.4
+    text.outlineColor = Color3.FromHexString('#8cfdff')
+
+    text.width = 20
+    text.height = 10
+    text.hTextAlign = 'center'
+
+    signText.addComponent(text)
+
+    signText.addComponent(
+      new Transform({
+        position: new Vector3(0.2, 0.475, -0.04),
+        rotation: Quaternion.Euler(0, 0, 0),
+        scale: new Vector3(0.05, 0.05, 0.05)
+      })
+    )
+
+    bubble.addComponent(
+      new OnPointerDown(e => {
+        if (e.hit.length < 8)
+          this.toggle(bubble, text, !bubbleData.isOpen, true)
+      })
+    )
+
+    // handle actions
+    channel.handleAction('open', ({ sender }) => {
+      this.toggle(bubble, text, true)
+    })
+    channel.handleAction('close', ({ sender }) => {
+      this.toggle(bubble, text, false)
+    })
+    channel.handleAction<ChangeTextType>('changeText', action => {
+      text.value = action.values.newText
+    })
+  }
+}
