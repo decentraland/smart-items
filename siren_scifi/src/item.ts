b1207ee8b6f5432c2f8ee005d0f27f3f278a68a9
nearnshaw <nearnshaw@decentraland.org>
2019-11-29 10:33:08 -0300

    sci fi siren first draft

---
+++ siren_scifi/src/item.ts
@@ -0,0 +1,86 @@
+export type Props = {
+  onActivate?: Actions
+  onDeactivate?: Actions
+}
+
+export default class Button implements IScript<Props> {
+  clip = new AudioClip('sounds/siren.mp3')
+
+  active: Record<string, boolean> = {}
+
+  init() {}
+
+  toggle(entity: Entity, value: boolean) {
+    if (this.active[entity.name] === value) return
+
+    if (value) {
+      const source = new AudioSource(this.clip)
+      entity.addComponentOrReplace(source)
+      source.loop = true
+      source.playing = true
+    } else {
+      const source = entity.getComponent(AudioSource)
+      if (source) {
+        source.playing = false
+      }
+    }
+
+    const animator = entity.getComponent(Animator)
+    const activateClip = animator.getClip('trigger')
+    const deactivateClip = animator.getClip('deactivate')
+    activateClip.stop()
+    deactivateClip.stop()
+    const clip = value ? activateClip : deactivateClip
+    clip.play()
+
+    this.active[entity.name] = value
+  }
+
+  spawn(host: Entity, props: Props, channel: IChannel) {
+    const siren = new Entity(host.name + '-button')
+    siren.setParent(host)
+
+    const animator = new Animator()
+    const deactivateClip = new AnimationState('deactivate', { looping: true })
+    const activateClip = new AnimationState('trigger', { looping: true })
+    animator.addClip(deactivateClip)
+    animator.addClip(activateClip)
+    siren.addComponent(animator)
+    deactivateClip.play()
+
+    siren.addComponent(new GLTFShape('models/Futuristic_Siren.glb'))
+
+    siren.addComponent(
+      new OnPointerDown(() => {
+        const value = !this.active[siren.name]
+        const action = channel.createAction(
+          value ? 'activate' : 'deactivate',
+          {}
+        )
+        channel.sendActions([action])
+      })
+    )
+
+    this.active[siren.name] = false
+
+    // handle actions
+    channel.handleAction('activate', ({ sender }) => {
+      this.toggle(siren, true)
+      if (sender === channel.id) {
+        channel.sendActions(props.onActivate)
+      }
+    })
+    channel.handleAction('deactivate', ({ sender }) => {
+      this.toggle(siren, false)
+      if (sender === channel.id) {
+        channel.sendActions(props.onDeactivate)
+      }
+    })
+
+    // sync initial values
+    channel.request<boolean>('isActive', isActive =>
+      this.toggle(siren, isActive)
+    )
+    channel.reply<boolean>('isActive', () => this.active[siren.name])
+  }
+}
