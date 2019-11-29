b1f13bf660d746624d1e0bc66c513741e61c80da
Juan Cazala <juan@decentraland.org>
2019-11-29 17:57:57 -0300

    fix: move types outside of the method

--- signpost_fantasy_triple/src/item.ts
+++ signpost_fantasy_triple/src/item.ts
@@ -5,8 +5,10 @@ export type Props = {
   fontSize?: number
 }
 
+type ChangeTextType = { newText: string }
+
 export default class SignPost implements IScript<Props> {
-  init() {}
+  init() { }
 
   spawn(host: Entity, props: Props, channel: IChannel) {
     const sign = new Entity()
@@ -112,7 +114,6 @@ export default class SignPost implements IScript<Props> {
       })
     )
 
-    type ChangeTextType = { newText: string }
     channel.handleAction<ChangeTextType>('changeTopText', action => {
       text1.value = action.values.newText
     })
--- signpost_pirates_double/src/item.ts
+++ signpost_pirates_double/src/item.ts
@@ -4,8 +4,10 @@ export type Props = {
   fontSize?: number
 }
 
+type ChangeTextType = { newText: string }
+
 export default class SignPost implements IScript<Props> {
-  init() {}
+  init() { }
 
   spawn(host: Entity, props: Props, channel: IChannel) {
     const sign = new Entity()
@@ -78,7 +80,6 @@ export default class SignPost implements IScript<Props> {
       })
     )
 
-    type ChangeTextType = { newText: string }
     channel.handleAction<ChangeTextType>('changeTopText', action => {
       text1.value = action.values.newText
     })
