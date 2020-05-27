c845014d04aa7ebc8c444e06d774e9c39751d919
nearnshaw <nearnshaw@decentraland.org>
2020-05-27 18:16:29 -0300

    spark

---
+++ spark/src/game.ts
@@ -0,0 +1,13 @@
+import { Spawner } from '../node_modules/decentraland-builder-scripts/spawner'
+import Spark, { Props } from './item'
+
+const arrow = new Spark()
+const spawner = new Spawner<Props>(arrow)
+
+spawner.spawn(
+  'spark',
+  new Transform({
+    position: new Vector3(4, 0, 8),
+  }),
+  { active: true }
+)
